24149480d9de5e9c6563b2ed0b200a9acf8007c8
Daniel Arnold <skadarnold@gmail.com>
2025-08-10 18:18:09 -0600

    Palette Class Optimization

--- palette.cpp
+++ palette.cpp
@@ -7,126 +7,209 @@
  */
  
 #include "stdafx.h"
-
 #include "palette.h"
 
-PalEntry *Palette::GetPalEntry(unsigned short which)
-{
-	if (which < 256)
-		return &palData[which];
+// ============================================================================
+// CONSTRUCTORS AND DESTRUCTOR
+// ============================================================================
 
-	return 0;
+Palette::Palette(void)
+{
+    initializeMembers();
 }
+
+Palette::~Palette(void)
+{
+    // No dynamic memory to clean up - palData is a fixed array
+    // CompPal Head is a simple struct
+}
+
+void Palette::initializeMembers()
+{
+    // Initialize Head structure
+    memset(&Head, 0, sizeof(Head));
+    
+    // Initialize default palette
+    initializeDefaultPalette();
+}
+
+void Palette::initializeDefaultPalette()
+{
+    // Initialize palette with original default colors
+    for (int i = 0; i < 256; i++)
+    {
+        if (i > 224)
+        {
+            palData[i].blue = i;
+            palData[i].green = i;
+        } 
+        else 
+        {
+            palData[i].blue = 224;
+            palData[i].green = 224;
+        }
+        
+        palData[i].red = 0;      // red=255 green=0 => violet
+        palData[i].remap = 0;
+    }
+}
+
+// ============================================================================
+// PALETTE DATA ACCESS
+// ============================================================================
+
+PalEntry* Palette::GetPalEntry(unsigned short which)
+{
+    if (which < 256)
+        return &palData[which];
+
+    return nullptr;
+}
+
 bool Palette::SetPalEntry(PalEntry value, unsigned short which)
 {
-	if (which < 256)
-	{
-		palData[which] = value;
+    if (which < 256)
+    {
+        palData[which] = value;
 
-		if (which < Head.startOffset)
-		{
-			Head.nColors += (Head.startOffset-which);
-			Head.startOffset = which;
-		} else if (which > Head.startOffset+Head.nColors)
-			Head.nColors = which-Head.startOffset;
+        // Update header information based on the entry position
+        if (which < Head.startOffset)
+        {
+            Head.nColors += (Head.startOffset - which);
+            Head.startOffset = which;
+        } 
+        else if (which > Head.startOffset + Head.nColors)
+        {
+            Head.nColors = which - Head.startOffset;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
+// ============================================================================
+// PALETTE MANAGEMENT
+// ============================================================================
 
-bool Palette::loadPalette(FILE *cfilebuf, unsigned long palsize)
+void Palette::noPalette() 
+{ 
+    // Set grayscale palette
+    for (int i = 0; i < 256; i++)
+    {
+        palData[i].blue = 255 - i;
+        palData[i].green = 255 - i;
+        palData[i].red = 255 - i;	
+        palData[i].remap = 0;
+    }
+}
+
+bool Palette::loadPalette(FILE* cfilebuf, unsigned long palsize)
 {
     // Check if the file buffer is valid
-    if (cfilebuf)
+    if (!cfilebuf)
+        return false;
+        
+    // Read the first 2 bytes of the file buffer
+    unsigned short tcheck = 0;
+    fread(&tcheck, 2, 1, cfilebuf);     
+    
+    // Check the value of tcheck, if it is not PALPATCH80 or PALPATCH
+    // then set the file pointer back by 2 bytes and read the header
+    if ((tcheck != PALPATCH80) && (tcheck != PALPATCH))
     {
-        // Read the first 2 bytes of the file buffer
-        unsigned short tcheck=0;
-        fread(&tcheck, 2, 1, cfilebuf);     
+        fseek(cfilebuf, -2, SEEK_CUR);
+        fread(&Head, COMPPALSIZE, 1, cfilebuf);
         
-        // Check the value of tcheck, if it is not PALPATCH80 or PALPATCH
-        // then set the file pointer back by 2 bytes and read the header
-        if ((tcheck != PALPATCH80) && (tcheck != PALPATCH))
-        {
-            fseek(cfilebuf, -2, SEEK_CUR);
-            //fread(&head, PalHeaderSIZE,1,cfilebuf);
+        // Check if the data length plus 15 is equal to the size of the palette
+        // If not, return false
+        // Dhel - not sure what this is used for, but using it now breaks p56 importing of different size images.
+        // if ((Head.reserved + 15) != palsize)    
+        //    return false;             
+    }
+    else
+    {
+        fread(&Head, COMPPALSIZE, 1, cfilebuf);
+    }
+    
+    // Allocate memory for the data based on the type value in the header
+    void* data = nullptr;
+    if (Head.type)
+    {
+        data = new PalEntryOld[Head.nColors];
+        fread(data, PalEntryOldSIZE, Head.nColors, cfilebuf);
+    }
+    else
+    {
+        data = new PalEntry[Head.nColors];
+        fread(data, PalEntrySIZE, Head.nColors, cfilebuf);
+    }
 
-			fread(&Head, COMPPALSIZE,1,cfilebuf);
-            
-            // Check if the data length plus 15 is equal to the size of the palette
-            // If not, return false
-			// Dhel - not sure what this is used for, but using it now breaks p56 importing of different size images.
-            // if ((Head.reserved + 15) != palsize)    
-            //    return false;             
-        }
-        else
-            fread(&Head, COMPPALSIZE,1,cfilebuf);
-        
-        // Allocate memory for the data based on the tripleColor value in the header
-        void *data = 0;
-        if (Head.type)
+    // Copy the data to the palData array based on the value of Head.type
+    if (!Head.type)
+    {
+        // New format - direct copy
+        for (int i = 0; i < Head.nColors; i++)
+            palData[i + Head.startOffset] = ((PalEntry*)data)[i];
+    }
+    else
+    {
+        // Old format - convert to new format
+        PalEntryOld tpalold;
+        for (int i = 0; i < Head.nColors; i++)
         {
-            data = new PalEntryOld[Head.nColors];
-            fread(data, PalEntryOldSIZE,Head.nColors,cfilebuf);
+            tpalold = ((PalEntryOld*)data)[i];
+            palData[i + Head.startOffset].red = tpalold.red;
+            palData[i + Head.startOffset].green = tpalold.green;
+            palData[i + Head.startOffset].blue = tpalold.blue;
+            palData[i + Head.startOffset].remap = 0;
         }
-        else
-        {
-            data = new PalEntry[Head.nColors];
-            fread(data, PalEntrySIZE,Head.nColors,cfilebuf);
-        }
+    }
 
-		// Copy the data to the palData array based on the value of (bool)(Head.type == 0)
-		if (!Head.type)
-			for (int i = 0; i < Head.nColors; i++)
-				palData[i + Head.startOffset] = ((PalEntry *)data)[i];
-		else
-		{
-			PalEntryOld tpalold;
-			for (int i = 0; i < Head.nColors; i++)
-			{
-				tpalold = ((PalEntryOld *)data)[i];
-				palData[i + Head.startOffset].red = tpalold.red;
-				palData[i + Head.startOffset].green = tpalold.green;
-				palData[i + Head.startOffset].blue = tpalold.blue;
-				palData[i + Head.startOffset].remap = 0;
-			}
-		}
+    // Clean up allocated memory
+    if (Head.type)
+        delete[] static_cast<PalEntryOld*>(data);
+    else
+        delete[] static_cast<PalEntry*>(data);
 
-		// set the _hasPalette variable to true indicating that a palette has been loaded
-		//_hasPalette = true;
-		// Return true indicating that the function has executed successfully
-		return true;
-	}
-	// Return false if the file buffer is invalid
-	return false;
+    // Return true indicating that the function has executed successfully
+    return true;
 }
 
-void Palette::WritePalette(FILE *cfb, bool writesciheader)
+void Palette::WritePalette(FILE* cfb, bool writesciheader)
 {
-	if (cfb && palData)
-	{
-				
-		unsigned long tsize = COMPPALSIZE + (Head.nColors * (!Head.type ?4 :3));
+    if (!cfb || !palData)
+        return;
+            
+    unsigned long tsize = COMPPALSIZE + (Head.nColors * (!Head.type ? 4 : 3));
 
-		unsigned short ttag = PALPATCH80;
-		if (writesciheader)
-			fwrite(&ttag, 2,1,cfb);
-		else
-		{
-			ttag = PALETTE_POS;
-			fwrite(&ttag, 2,1,cfb);
-			fwrite(&tsize, 4,1,cfb);
-		}
+    unsigned short ttag = PALPATCH80;
+    if (writesciheader)
+    {
+        fwrite(&ttag, 2, 1, cfb);
+    }
+    else
+    {
+        ttag = PALETTE_POS;
+        fwrite(&ttag, 2, 1, cfb);
+        fwrite(&tsize, 4, 1, cfb);
+    }
 
-		fwrite(&Head, COMPPALSIZE, 1, cfb);
-		for (int i=0;i<Head.nColors;i++)
-			if (!Head.type)
-				fwrite(&(palData[i+Head.startOffset]), PalEntrySIZE, 1, cfb);
-			else
-				fwrite(((char*)&(palData[i+Head.startOffset]))+1, PalEntryOldSIZE, 1, cfb);
-	
-	}
-
+    fwrite(&Head, COMPPALSIZE, 1, cfb);
+    
+    // Write palette entries based on type
+    for (int i = 0; i < Head.nColors; i++)
+    {
+        if (!Head.type)
+        {
+            // New format - write full PalEntry
+            fwrite(&(palData[i + Head.startOffset]), PalEntrySIZE, 1, cfb);
+        }
+        else
+        {
+            // Old format - write only RGB (skip remap byte)
+            fwrite(((char*)&(palData[i + Head.startOffset])) + 1, PalEntryOldSIZE, 1, cfb);
+        }
+    }
 }
--- palette.h
+++ palette.h
@@ -11,119 +11,172 @@
 
 #pragma pack(1)
 
+// ============================================================================
+// PALETTE ENTRY STRUCTURES
+// ============================================================================
+
 struct PalEntryOld
 {
-	unsigned char red;
-	unsigned char green;
-	unsigned char blue;
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
 };
-#define PalEntryOldSIZE (sizeof(PalEntryOld))
 
 struct PalEntry
 {
-	unsigned char remap;
-	unsigned char red;
-	unsigned char green;
-	unsigned char blue;
+    unsigned char remap;
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
 };
-#define PalEntrySIZE (sizeof(PalEntry))
 
 struct BMPColorHead
 {
-	unsigned char red;
-	unsigned char green;
-	unsigned char blue;
-	unsigned char alpha;
-	unsigned char color_space_type;
-	unsigned char unused;
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+    unsigned char alpha;
+    unsigned char color_space_type;
+    unsigned char unused;
 };
-#define BMPColorHeadSize (sizeof(BMPColorHead))
+
+// ============================================================================
+// PALETTE HEADER STRUCTURES
+// ============================================================================
 
 struct PalHeader
 {
-	short palID;
-	char hdSize;
-	char palName[9];
-	char palCount;
-	short reserved;
+    short palID;
+    char hdSize;
+    char palName[9];
+    char palCount;
+    short reserved;
 };
-
-const int PALHEADERSIZE = sizeof(PalHeader);
 
 struct CompPal : public PalHeader
 {
-	char title[10]; //  8 chars, 0 terminated
-	uchar startOffset;
-	uchar nCycles;	//  number of cycling ranges following header
-	UInt16 fe;		//  future expansion (0)
-	UInt16 nColors; //  number of "colors" defined in this palette
-	uchar def;		//  "Default" flag setting (1)
-	uchar type;		//  (0 = each RGB has flag)
-					//  (1 = All RGBs share default flag)
-	UInt32 valid;
+    char title[10];         // 8 chars, 0 terminated
+    uchar startOffset;
+    uchar nCycles;          // number of cycling ranges following header
+    UInt16 fe;              // future expansion (0)
+    UInt16 nColors;         // number of "colors" defined in this palette
+    uchar def;              // "Default" flag setting (1)
+    uchar type;             // (0 = each RGB has flag)
+                           // (1 = All RGBs share default flag)
+    UInt32 valid;
 };
-
-const int COMPPALSIZE = sizeof(CompPal);
-
-#define PALPATCH80    0x008B
-#define PALPATCH      0x000B
-
-
-#define PALETTE_POS 0x0300
 
 #pragma pack()
 
+// ============================================================================
+// SIZE CONSTANTS
+// ============================================================================
+
+#define PalEntryOldSIZE (sizeof(PalEntryOld))
+#define PalEntrySIZE (sizeof(PalEntry))
+#define BMPColorHeadSize (sizeof(BMPColorHead))
+#define PALHEADERSIZE (sizeof(PalHeader))
+#define COMPPALSIZE (sizeof(CompPal))
+
+// ============================================================================
+// FORMAT CONSTANTS
+// ============================================================================
+
+#define PALPATCH80    0x008B
+#define PALPATCH      0x000B
+#define PALETTE_POS   0x0300
+
+/**
+ * @brief Palette class for handling SCI palette data
+ * 
+ * This class manages palette information including loading from files,
+ * color management, and writing back to SCI format files.
+ */
 class Palette
 {
-	
 public:
-	Palette(void)
-	{ for (int i = 0; i<256; i++)
-	  {
-          if (i>224)
-          {
-		      palData[i].blue = i;//255;
-		      palData[i].green = i;//255;
-		  
-          } else {
-              palData[i].blue = 224;
-		      palData[i].green = 224;
-          }
-          
-          palData[i].red = 0;	//red=255  green=0  => violet
-		  palData[i].remap = 0;
-	  }
-    /*  palData[255].blue = 0;// 255;
-	  palData[255].green = 255;
-	  palData[255].red =255;//red=255  green=0  => violet
-	  palData[255].remap = 0;    */
+    // ============================================================================
+    // CONSTRUCTORS AND DESTRUCTOR
+    // ============================================================================
+    
+    /**
+     * @brief Default constructor
+     */
+    Palette(void);
+    
+    /**
+     * @brief Destructor
+     */
+    ~Palette(void);
+    
+    // Prevent copying to avoid issues with the large palData array
+    Palette(const Palette&) = delete;
+    Palette& operator=(const Palette&) = delete;
+    
+    // ============================================================================
+    // PALETTE DATA ACCESS
+    // ============================================================================
+    
+    /**
+     * @brief Get a palette entry by index
+     * @param which Index of the palette entry (0-255)
+     * @return Pointer to palette entry or null if index invalid
+     */
+    PalEntry* GetPalEntry(unsigned short which);
+    
+    /**
+     * @brief Set a palette entry by index
+     * @param value New palette entry value
+     * @param which Index to set (0-255)
+     * @return True if successful, false if index invalid
+     */
+    bool SetPalEntry(PalEntry value, unsigned short which);
+    
+    // ============================================================================
+    // PALETTE MANAGEMENT
+    // ============================================================================
+    
+    /**
+     * @brief Load palette from file buffer
+     * @param cfilebuf File buffer to read from
+     * @param palsize Size of palette data
+     * @return True if successful, false otherwise
+     */
+    bool loadPalette(FILE* cfilebuf, unsigned long palsize);
+    
+    /**
+     * @brief Set palette to grayscale default
+     */
+    void noPalette();
+    
+    /**
+     * @brief Write palette to file
+     * @param cfb File buffer to write to
+     * @param writesciheader Whether to write SCI header
+     */
+    void WritePalette(FILE* cfb, bool writesciheader);
+    
+    // ============================================================================
+    // PUBLIC MEMBER DATA
+    // ============================================================================
+    
+    PalEntry palData[256];          // Palette data array
+    CompPal Head;                   // Palette header
 
-	}
-	//~Palette(void);
-
-	PalEntry palData[256];
-
-    //bool _hasPalette;
-
-	CompPal Head;
-	
-	PalEntry *GetPalEntry(unsigned short which);
-	bool SetPalEntry(PalEntry value, unsigned short which);
-
-	bool loadPalette(FILE *cfilebuf, unsigned long palsize);
-    void noPalette() { 
-                       for (int i = 0; i<256; i++)
-	                   {
-		                    palData[i].blue = 255-i;
-		                    palData[i].green = 255-i;
-		                    palData[i].red = 255-i;	
-		                    palData[i].remap = 0;
-	                   }
-                       //_hasPalette = false;
-                     }
-
-	void WritePalette(FILE *cfb, bool writesciheader);
-
+private:
+    // ============================================================================
+    // PRIVATE HELPER METHODS
+    // ============================================================================
+    
+    /**
+     * @brief Initialize member variables to safe defaults
+     */
+    void initializeMembers();
+    
+    /**
+     * @brief Initialize default palette colors
+     */
+    void initializeDefaultPalette();
 };
 
-#endif
+#endif // PALETTE_H
